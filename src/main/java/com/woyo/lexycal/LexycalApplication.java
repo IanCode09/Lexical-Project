20a64047b5fd386c465cee8ee13f64c95f3ac1f9
Ian Lombu <citrakemurnianlombu@gmail.com>
2022-05-03 22:26:32 +0700

    First Commit

---
+++ src/main/java/com/woyo/lexycal/LexycalApplication.java
@@ -0,0 +1,13 @@
+package com.woyo.lexycal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexycalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexycalApplication.class, args);
+	}
+
+}
